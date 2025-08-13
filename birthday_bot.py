357c508731d6bc1026a64927aff92eab21fb9240
dmitrysb1976-prog <dmitrysb1976@gmail.com>
2025-08-13 20:56:30 +0200

    Update birthday_bot.py

--- birthday_bot.py
+++ birthday_bot.py
@@ -83,11 +83,11 @@ async def check_birthdays(sheet):
                 continue
 
         if birthday_people:
-            if len(birthday_people) > 1:
-                names_str = ", ".join(birthday_people)
-                message = f"Наш именинник сегодня: {names_str} - поздравляем с Днем Рождения!!! ❤️"
+            if len(birthday_people) == 1:
+                message = f"Наш именинник сегодня: {birthday_people[0]} - поздравляем с Днем Рождения!!! ❤️"
             else:
-                message = f"Наши именинники сегодня: {birthday_people[0]} - поздравляем с Днем Рождения!!! ❤️"
+                names_str = ", ".join(birthday_people)
+                message = f"Наши именинники сегодня: {names_str} - поздравляем с Днем Рождения!!! ❤️"
 
             await bot.send_message(chat_id=CHANNEL_ID, text=message)
             logger.info(f"Отправлено сообщение: {message}")
@@ -125,6 +125,3 @@ if __name__ == "__main__":
         logger.info("Бот остановлен вручную")
     except Exception as e:
         logger.critical(f"Необработанное исключение: {str(e)}")
-
-
-
